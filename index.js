e75fc8aeee64147a165fcc57f3126d4092d433b3
ringolol <57327672+ringolol@users.noreply.github.com>
2020-02-25 18:27:49 +0300

    test_sql

--- index.js
+++ index.js
@@ -70,51 +70,72 @@ bot.on('message', async message => {
         message.channel.send(msg)
     }
 
-    if(!userLvl[uid]) {
-        userLvl[uid] = {
-            nickname: nickname,
-            username: username,
-            coins: 10,
-            warns: 0,
-            xp: 0,
-            lvl: 0,
-            questions: {
-                question: [],
-                answer: []
-            }
-        }
-    }
+    // if(!userLvl[uid]) {
+    //     userLvl[uid] = {
+    //         nickname: nickname,
+    //         username: username,
+    //         coins: 10,
+    //         warns: 0,
+    //         xp: 0,
+    //         lvl: 0,
+    //         questions: {
+    //             question: [],
+    //             answer: []
+    //         }
+    //     }
+    // }
 
-    const u = userLvl[uid];
-    u.coins++;
-    u.xp++;
-    if(u.xp >= (u.lvl * 5)) {
-        u.xp = 0;
-        u.lvl += 1;
-    }
+    sql_add_user = 
+    `INSERT IGNOR INTO users (user_id, user_name, server_name)
+        VALUES (${uid}, ${username}, ${nickname})`;
 
-    fs.writeFile('./user_lvl.json', JSON.stringify(userLvl), (err) => {
-        if(err){
-            console.log(err)
-        }
-    })
+    console.log('sql_add_user:\n' + sql_add_user);
+
+    connection.query(sql_add_user, function (error, results, fields) {
+        console.log('error: ' + error);
+        console.log(results);
+        // console.log(fields);
+    });
+
     
-    if(message.channel.id === '677624287649333268' || message.channel.id === '678701864514224170' || message.channel.id === '624327775935004687') {
-        if(/Нода дай монеток$|!монетки/i.test(message.content)){
-            u.coins += 100;
-            let pushCoins = new RichEmbed()
-            .setTitle(`Запрос халявных монеток`)
-            .setColor(0x36D904)
-            .setDescription(`
-            Держи 100 монеток :moneybag:
-            Чеканных монет: ${userLvl[uid].coins} 
-            `);
-            message.channel.send(pushCoins);
-        }
-    }
+
+    // const u = userLvl[uid];
+    // u.coins++;
+    // u.xp++;
+
+    sql_add_coins_exp = 
+    `UPDATE users
+        SET coins = coins + 1, exp = exp + 1
+        WHERE user_id = ${uid}`;
+
+    // if(u.xp >= (u.lvl * 5)) {
+    //     u.xp = 0;
+    //     u.lvl += 1;
+    // }
+
+    // fs.writeFile('./user_lvl.json', JSON.stringify(userLvl), (err) => {
+    //     if(err){
+    //         console.log(err)
+    //     }
+    // })
+    
+    // if(message.channel.id === '677624287649333268' || message.channel.id === '678701864514224170' || message.channel.id === '624327775935004687') {
+    //     if(/Нода дай монеток$|!монетки/i.test(message.content)){
+    //         u.coins += 100;
+    //         let pushCoins = new RichEmbed()
+    //         .setTitle(`Запрос халявных монеток`)
+    //         .setColor(0x36D904)
+    //         .setDescription(`
+    //         Держи 100 монеток :moneybag:
+    //         Чеканных монет: ${userLvl[uid].coins} 
+    //         `);
+    //         message.channel.send(pushCoins);
+    //     }
+    // }
     
 });
 
+die("STOP")
 // Блок с вопросами боту
 
 bot.on('message', async message => {
