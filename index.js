222175c9272597920ebdf996188a55e8cdbada44
ringolol <57327672+ringolol@users.noreply.github.com>
2020-03-03 18:40:11 +0300

    log_everything

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const sql_add_user =
 const sql_get_user_info =
 `SELECT * FROM users
     WHERE user_id = ?`;
+
 const sql_get_conn_quest_ans_info =
 `SELECT * FROM conn_quest_ans
     WHERE user_id = ?`;
+
 const sql_upd_user_info = 
 `UPDATE users
     SET coins = ?, exp = ?, lvl = ?, questions = ?
@@ -35,6 +37,7 @@ const sql_find_question =
     ORDER BY score DESC LIMIT 100`;
 
 // require
+console.log('Noda / Start...');
 const Discord = require('discord.js');
 const config = require('./config.json');
 const fs = require('fs');
@@ -42,31 +45,30 @@ const mysql = require('mysql');
 const util = require('util');
 const { Attachment, RichEmbed, Emoji, Guild, Client } = require('discord.js');
 
-// bot vars
-const bot = new Discord.Client();
-bot.commands = new Discord.Collection();
-const token = config.token;
-const prefix = config.prefix;
-
-// login bot
-bot.login(token);
-
 // require modules
 fs.readdir('./modules/',(err,files)=>{
     if(err) console.log(err);
     let jsfiles = files.filter(f => f.split(".").pop() === "js");
-    if(jsfiles.length <=0) console.log("Нет комманд для загрузки!!");
-    console.log(`Загружено ${jsfiles.length} комманд`);
+    if(jsfiles.length <=0) console.log("Noda / Nothing to load");
+    console.log(`Noda / LM / Loading ${jsfiles.length} module(s)`);
     jsfiles.forEach((f,i) =>{
         let props = require(`./modules/${f}`);
-        console.log(`${i+1}.${f} Загружен!`);
+        console.log(`Noda / LM / ${f} module is loaded`);
         for(let i = 0; i < props.help.name.length; i++){
             bot.commands.set(props.help.name[i],props);
         }
     });
 });
 
-// Regex
+console.log('Noda / All modules are connected');
+
+// bot vars
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+const token = config.token;
+const prefix = config.prefix;
+
+// regex
 const buy_question = new RegExp(prefix + '\\купить вопрос$','i');
 const buy_common_question = new RegExp(prefix + '\\общий вопрос$','i');
 const buy_personal_question = new RegExp(prefix + '\\личный вопрос$','i');
@@ -74,6 +76,9 @@ const just_question = new RegExp(prefix + '\\вопрос','i');
 const show_profile = new RegExp(prefix + '\\профиль$|^нода покажи мой профиль','i');
 const personal_question = /^личный$/i;
 const common_question = /^общий$/i;
+
+// login bot
+bot.login(token);
 
 // bot.on('ready', async () => {
 //     console.log(`Нода тян запущена`);
@@ -83,6 +88,8 @@ const common_question = /^общий$/i;
 //         console.log(err.stack);
 //     })
 // });
+
+console.log('Noda / Bot initialized');
 
 // russian stemming
 // npm i natural
@@ -95,19 +102,27 @@ function stemming(str) {
         stems.push(natural.PorterStemmerRu.stem(word));
     }
     str_stemmed = stems.join(' ')
-    console.log(`stemmed: ${str_stemmed}`);
+    console.log(`Noda / Stemmed / ${str_stemmed}`);
     return str_stemmed;
 }
 
+console.log('Noda / MSG / Start listening');
 // Handle messages
 bot.on('message', async message => {
     try {
+        console.time('Noda / MSG / TIME');
+        console.log('Noda / MSG / Handle new message');
+        console.log('===================================================');
         // don't handle messages from bots
-        if(message.author.bot) return;
+        if(message.author.bot)
+        {
+            console.log('Noda / MSG / Author is a bot');
+            return;
+        }
+        console.log(`Noda / MSG / Message text: '${message.content}'`);
+            
         // don't handle direct messages
         // if(message.channel.type === "dm") return;
-
-        console.log('===================================================');
     
         // user info from discord
         const uid = message.author.id;
@@ -125,6 +140,7 @@ bot.on('message', async message => {
             message.channel.send(msg)
         }
 
+        console.log('Noda / MSG / Create MySQL connection');
         // connection info
         const connection  = mysql.createConnection({
             host:       config.DB.host,
@@ -138,28 +154,31 @@ bot.on('message', async message => {
     
         // handle errors
         connection.on('error', function() {
-            console.log('Connection cancelled due to timeout or another error');
+            console.log('Noda / MSG / Connection cancelled due to timeout or another error');
         });
 
+        console.log('Noda / MSG / Connect to MySQL DB');
         // connect to database
         connection.connect(function(err) {
             if (err) {
-                console.error('DB / database connection error: ' + err.stack);
+                console.error('Noda / MSG / Database connection error: ' + err.stack);
                 return;
             }
-            console.log('DB / connected as id ' + connection.threadId);
+            console.log('Noda / MSG / Connected to DB as id ' + connection.threadId);
         });
 
-        // add user if needed ( may not handle nick change )
+        // add user if needed
+        console.log('Noda / MSG / Add user into DB if needed ( may not handle nick change )');
         await query(sql_add_user, [uid, username, nickname]);
 
         // get user info from DB
+        console.log('Noda / MSG / Get user info from DB');
         const user_data = await query(sql_get_user_info, [uid]);
-        console.log('User Data: ');
-        console.log(user_data);
+        // console.log(user_data[0]);
 
         if (user_data) {
             // User Data
+            console.log('Noda / MSG / Parse user data');
             let coins = user_data[0]['coins'];
             let exp = user_data[0]['exp'];
             let lvl = user_data[0]['lvl'];
@@ -169,9 +188,13 @@ bot.on('message', async message => {
             let question_num = user_data[0]['questions'];
             let question = null;
             let answer = null;
+            console.log(`Noda / MSG / User data: \n\tuser_name: '${username}'\n\tnickname: '${nickname}'` + 
+                `\n\tuser_id: '${uid}'\n\tcoins: '${coins}'\n\tlvl: '${lvl}'\n\texp: '${exp}'\n\tquestions: '${question_num}'`);
             
-            // System command
+            // system commands
+            console.log('Noda / MSG / HM / Handle message');
             if(/^нода$/i.test(message.content)){
+                console.log(`Noda / MSG / HM / Bot's name`);
                 let randomNumber = Math.ceil(Math.random() * 10);
                 switch(randomNumber){
                     case 1:
@@ -202,8 +225,9 @@ bot.on('message', async message => {
                         message.channel.send('Как банный лист пристал...');
                         break;
                 }
-                // Bye questions guide
+            // buy questions guide
             } else if(buy_question.test(message.content)){
+                console.log(`Noda / MSG / HM / Buy question info`);
                 const shop = new RichEmbed()
                     .setTitle(`Нода-шоп!`)
                     .setColor(0xebe134)
@@ -216,8 +240,9 @@ bot.on('message', async message => {
                     Для покупки личного вопроса напишите: !личный вопрос
                     `);
                 message.channel.send(shop);
-                // Bye common_questions guide
+            // buy common_questions guide
             } else if(buy_common_question.test(message.content)){
+                console.log(`Noda / MSG / HM / Buy common question info`);
                 if(coins >= 25){
                     const commonQuestion = new RichEmbed()
                     .setTitle(`Покупка общего вопроса.`)
@@ -237,8 +262,9 @@ bot.on('message', async message => {
                     `);
                     message.channel.send(commonQuestionFalse);
                 }  
-                // Bye personal_question guide 
+            // buy personal_question guide 
             } else if(buy_personal_question.test(message.content)){
+                console.log(`Noda / MSG / HM / Buy personal question info`);
                 if(coins >= 100){
                     let plate = new RichEmbed()
                     .setTitle(`Покупка личного вопроса.`)
@@ -258,10 +284,12 @@ bot.on('message', async message => {
                     `);
                     message.channel.send(plate_false);
                 }  
-                // Bye questions code
+            // buy questions with code
             } else if(just_question.test(message.content)){
+                console.log(`Noda / MSG / HM / BQ / Buy a question!`);
                 let args = message.content.split(" [");
                 if (coins >= 25 && args.length >= 2) {
+                    console.log(`Noda / MSG / HM / BQ / Enough money for a common question and the question has proper structure`);
                     question = args[1].slice(0, -1);
                     answer = args[2].slice(0, -1);
                     try {
@@ -269,22 +297,25 @@ bot.on('message', async message => {
                     } catch(error) {
                         question_type = 0;
                     }
-                    console.log(question);
-                    console.log(question_type);
+                    console.log(`Noda / MSG / HM / BQ / Question type ${question_type}`);
                     if(personal_question.test(question_type)){
                         if(coins >= 100) {
+                            console.log(`Noda / MSG / HM / BQ / Personal question was bought`);
                             question_type = 1;
                             coins -= 100;
                             console.log(question_type);
                         } else {
+                            console.log(`Noda / MSG / HM / BQ / Not enough money for personal question`);
                             bot.send(`Не хватает чеканных монет для покупки личного вопроса.\nВаш баланс: ${coins} монет!`)
                             return;
                         }
                     } else if(question_type === 0 || common_question.test(question_type)){
+                        console.log(`Noda / MSG / HM / BQ / Common question was bought`);
                         question_type = 0;
                         coins -= 25;
                         console.log(question_type);
                     } else {
+                        console.log(`Noda / MSG / HM / BQ / Incorrect question type`);
                         let plate = new RichEmbed()
                         .setTitle(`Ошибка`)
                         .setColor(0xFF0000)
@@ -297,6 +328,7 @@ bot.on('message', async message => {
                         return;
                     }
                     question_num += 1;
+                    console.log(`Noda / MSG / HM / BQ / Q: '${question}', A: '${answer}', T: '${question_type}'`);
                     const commonQuestionBye = new RichEmbed()
                     .setTitle(`Покупка оформлена.`)
                     .setColor(0x36D904)
@@ -309,10 +341,12 @@ bot.on('message', async message => {
                     `);
                     message.channel.send(commonQuestionBye);
                 } else {
+                    console.log(`Noda / MSG / HM / Not enough money for a common question`);
                     message.channel.send(`${args.length >= 2 ? 'Не хватает чеканных монет, ваш баланс:' + coins : 'Вы неправильно написали шаблон для покупки вопроса, можете посмотреть правильные шаблоны, написав "!купить общий вопрос" или "!купить личный вопрос"'}`);
                 }
-                // Show profiles
+            // Show profiles
             } else if (show_profile.test(message.content)) {
+                console.log(`Noda / MSG / HM / Show profiles`);
                 if(message.member.nickname === null){
                     let embed = new RichEmbed()
                     .setTitle(`Профиль игрока: ${username}`)
@@ -338,14 +372,17 @@ bot.on('message', async message => {
                     .setThumbnail(avatar)
                     message.channel.send(embed);
                 }
-                // Throw a cube
+            // Throw a cube
             } else if(message.content === '!кубик') {
+                console.log(`Noda / MSG / HM / Throw a cube`);
                 message.channel.send(Math.ceil(Math.random() * 10)); 
             } else {
-                // "Нода ..."
+                // "Нода ..." или "!..."
                 if (/^Нода|^!/i.test(message.content)) {
+                    console.log(`Noda / MSG / HM / QN / Question to Noda`);
                     // "Нода дай монет"
                     if (/Дай монет|монетки/i.test(message.content)) {
+                        console.log(`Noda / MSG / HM / QN / Give some coins`);
                         // give 100 coins
                         coins += 100;
                         let pushCoins = new RichEmbed()
@@ -357,9 +394,11 @@ bot.on('message', async message => {
                         `);
                         message.channel.send(pushCoins);
                     } else {
-                        // find the closes questions in DB
+                        console.log(`Noda / MSG / HM / QN / Find the question in DB`);
+                        // find the closest questions in DB
                         matched_questions = await query(sql_find_question, [stemming(message.content)]);
-                        personal_question_check = await query(sql_get_conn_quest_ans_info, [uid]);
+                        // TEST
+                        /*personal_question_check = await query(sql_get_conn_quest_ans_info, [uid]);
                         let check_id = 0;
                         try {
                             check_id = personal_question_check[0]['user_id'];
@@ -368,17 +407,27 @@ bot.on('message', async message => {
                         }
                         console.log(personal_question_check);
                         console.log(`user id = ${check_id}`);
-                        console.log(uid == check_id);
+                        console.log(uid == check_id);*/
+
                         // if questions exist
                         if(matched_questions) {
-                            console.log('question_type :' + matched_questions[0]['type']);
+                            console.log(`Noda / MSG / HM / QN / There are some question in DB`);
+                            // console.log('question_type :' + matched_questions[0]['type']);
                             // maximum score to float
                             let type = matched_questions[0]['type'];
                             let max_score = parseFloat(matched_questions[0]['score']);
                             let ans = '';
                             // if max score greater than 0
                             if(max_score > 0) {
-                                if(type === 1) {
+                                console.log(`Noda / MSG / HM / QN / Top 5 matched questions`);
+                                if(matched_questions.length > 5) {
+                                    console.log(matched_questions.slice(0,5));
+                                } else {
+                                    console.log(matched_questions);
+                                }
+                                console.log(`Noda / MSG / HM / QN / Choose the top answer`);
+                                ans = matched_questions[0]['answer'];
+                                /*if(type === 1) {
                                    if(uid == check_id) {
                                         ans = matched_questions[0]['answer'];    
                                    } else {
@@ -386,8 +435,10 @@ bot.on('message', async message => {
                                    }
                                 } else {
                                     ans = matched_questions[0]['answer'];
-                                }       
+                                }*/   
                             } else {
+                                console.log(`Noda / MSG / HM / QN / No matches with questions in DB`);
+                                console.log(`Noda / MSG / HM / QN / Choose a random answer`);
                                 // no similar questions in DB
                                 let randomNumber = Math.ceil(Math.random() * 5);
                                 switch(randomNumber){
@@ -408,14 +459,16 @@ bot.on('message', async message => {
                                         break;
                                 }
                             }
+                            console.log(`Noda / MSG / HM / QN / Return the chosen answer`);
                             // answer
                             message.channel.send(ans);
                         }
-                        console.log('Matched questions');
-                        console.log(matched_questions);
+                        // console.log('Matched questions');
+                        // console.log(matched_questions);
                     }
                 }
                 // update coins, exp and lvl
+                console.log(`Noda / MSG / HM / Add coins, exp and upd lvl`);
                 coins += 1;
                 exp += 1;
                 if (exp >= lvl * 5) {
@@ -425,9 +478,11 @@ bot.on('message', async message => {
             }
 
             // update user info in DB
+            console.log(`Noda / MSG / HM / Update user data in DB`);
             await query(sql_upd_user_info, [coins, exp, lvl, question_num, uid]);
             // if the user created a question
             if (question && answer) {
+                console.log(`Noda / MSG / HM / Add bought question into DB`);
                 // add question to table questions
                 var add_question = await query(sql_add_question, [stemming(question)]);
                 var question_id = add_question.insertId;
@@ -440,22 +495,24 @@ bot.on('message', async message => {
                 await query(sql_connect_question, [question_id, answer_id, uid, question_type]);
 
                 // close connection to DB
-                console.log('DB / disconnected');
+                console.log('Noda / MSG / Disconnected from DB');
                 connection.end();
             } else {
                 // close connection to DB
-                console.log('DB / disconnected');
+                console.log('Noda / MSG / Disconnected from DB');
                 connection.end();
             }
         }
     // handle errors
     } catch (error) {
+        console.log('Noda / MSG / ERROR');
         console.log(error);
         if(connection) {
-            console.log('DB / disconnected');
+            console.log('Noda / MSG / Disconnected from DB');
             connection.end();
         }
-         
+    } finally {
+        console.timeEnd('Noda / MSG / TIME');
     }
 });
 
