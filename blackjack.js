95388fb752e6c41e3a95c7268b84faa940c111aa
ringolol <57327672+ringolol@users.noreply.github.com>
2020-03-05 18:36:37 +0300

    blackjack_in_console

--- blackjack.js
+++ blackjack.js
@@ -1,17 +1,41 @@
-const natural = require('natural');
-const queries = require('./queries');
+const black_jack = require('engine-blackjack');
+const util = require('util');
+const actions = black_jack.actions;
+const Game = black_jack.Game;
+const prompt = require('prompt-sync')({sigint: true});
 
-const black_jack = require('engine-blackjack')
-const actions = black_jack.actions
-const Game = black_jack.Game
+const game = new Game();
 
+while(true) {
+    let state = game.getState();
+    // console.log('======================================================================');
+    // console.dir(state);
+    // console.log('======================================================================');
+    console.log(`stage: ${state.stage}, won: ${state.wonOnRight}, final win: ${state.finalWin}, initial bet: ${state.initialBet}, final bet: ${state.finalBet}`)
+    console.log('Your hand: ')
+    console.log(state.handInfo.right.cards);
+    console.log('Dealer hand: ')
+    console.log(state.dealerCards);
+    console.log('Dealer hole card: ');
+    console.log(state.dealerHoleCard);
 
-async function blackjack(message, bot) {
-    const game = new Game('♠10 ♦1 ♥5 ♣6 ♠11 ♦10')
-    if (message.content == 'тест') {
-        // message.channel.send(game.state);
-        console.log(game.state);
+    const cmd = prompt('Your turn: ');
+    switch(cmd) {
+        // раздать карты
+        case 'deal':
+            game.dispatch(actions.deal(10))
+            break;
+        // забрать пол ставки и сдаться
+        case 'surrender':
+            game.dispatch(actions.surrender())
+            break;
+        // взять карту
+        case 'hit':
+            game.dispatch(actions.hit('right'))
+            break;
+        // больше карт не нужно
+        case 'stand':
+            game.dispatch(actions.stand('right'))
+            break;
     }
 }
-
-module.exports.game = blackjack;
