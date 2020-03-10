baa68e4ee7034ce5351be82a39ab4d825556a048
ringolol <57327672+ringolol@users.noreply.github.com>
2020-03-10 07:11:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const token = 'NjgxOTQxNTk1NjcwNTc3MTg4.Xmb-UA.zLUfCjJETCUFPWO3HwsbTpBw-6s';
 
 const DB = {    host: 'localhost',
                     user: 'root',
-                    password: '', 
+                    // password: '', 
                     database: 'mydb',
                     acquireTimeout:     1000000,
                     connectTimeout:     20000,
