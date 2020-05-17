caab81b9c0a4c65cdf32aa0f451cc6204f609fc6
Ringolol <createsite108@gmail.com>
2020-05-17 22:07:49 +0100

    commit message

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const token = 'NjgxOTQxNTk1NjcwNTc3MTg4.Xmb-UA.zLUfCjJETCUFPWO3HwsbTpBw-6s';
 
 const DB = {    host: 'localhost',
-                    user: 'root',
-                    password: 'password', 
-                    database: 'mydb',
-                    acquireTimeout:     1000000,
-                    connectTimeout:     20000,
-                    supportBigNumbers: true,
-                    bigNumberStrings: true    
-                };
+                user: 'root',
+                password: '', 
+                database: 'mydb',
+                acquireTimeout:     1000000,
+                connectTimeout:     20000,
+                supportBigNumbers: true,
+                bigNumberStrings: true    
+};
 
 module.exports = {token, DB};
     
